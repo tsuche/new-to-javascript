1597234f8bfae8bf54c7b7dc2cd88e1e7bd3d68d
tsuche <67209810+tsuche@users.noreply.github.com>
2020-07-11 22:08:11 -0700

    Learned variables & the length string property

---
+++ variable&properties.js
@@ -0,0 +1,60 @@
+console.log("Welcome to Javascript");
+
+// used for comments in-line
+
+/* Multiline
+Comments 
+*/
+
+VARIABLES:
+ 
+-var is global and can be altered
+ 
+var hisName = "Norman"
+hisName = "Ray"
+
+
+-const can't be altered 
+
+const herName = "Lauren"
+//herName cannot be set to another string besides Lauren
+
+-not too sure about Let yet
+
+
+\n ( new line )
+\t ( tab )
+
+var birthday = "January\n\t1st\n\t\t20202"
+//it take up 3 lines:
+/* 
+Januray
+  1st
+    20202
+    */
+    
+    
+PROPERTIES:
+  var firstName = "Louis"
+  var totalLetterInFirstName = firstName.length
+  console.log(totalLetterInFirstName);
+    // -> 5
+    
+console.log("How many letters are in Kimberly");
+
+  var kimberlyName = "Kimberly"
+  var totalLettersInKimberly = kimberlyName + "'s name " "has" + kimberlyName.length + "letters"
+  
+  console.log(totalLettersInKimberly);
+  // -> Kimberly's name has 8 letters
+  
+  
+  
+    
+    
+    
+
+  
+
+
+ 
