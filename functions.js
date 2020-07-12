905a1ec961b04cd36861755685315a0bb11793eb
tsuche <67209810+tsuche@users.noreply.github.com>
2020-07-12 02:46:36 -0700

    GLOBAL & LOCAL VARIABLES

--- functions.js
+++ functions.js
@@ -19,3 +19,26 @@ PARAMETERS
 
 testing(3,6);
 //->18
+
+
+-------
+  
+ GLOBAL AND LOCAL VARIABLES
+ 
+ //Global and Local Variables
+
+var myShoes = "vans";
+
+function favShoes (){
+    var myShoes = "Converse";
+    
+    return(myShoes);
+    //it's just talking in your calculation, and returning it back to you
+} 
+
+console.log(favShoes());
+//-> Converse
+console.log(myShoes);
+//->Vans
+
+-------
