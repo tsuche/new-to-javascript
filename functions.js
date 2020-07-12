08c2cad08fe309cf86d8c625c463103e588139da
tsuche <67209810+tsuche@users.noreply.github.com>
2020-07-12 02:07:32 -0700

    The basic structure of a function

---
+++ functions.js
@@ -0,0 +1,13 @@
+FUNCTIONS BASICS
+
+
+FUNCTIONS functionName(){
+  console.log("Hello Functions");
+ }
+ 
+ //you need to declare it to use by:
+ 
+ functionName();
+ //-> Hello Function
+ 
+ ------
