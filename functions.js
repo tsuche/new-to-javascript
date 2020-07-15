9581caebdcebfe652c74b29a7de124ab22e1c18b
tsuche <67209810+tsuche@users.noreply.github.com>
2020-07-14 23:21:54 -0700

    SWITCH AND CASES

--- functions.js
+++ functions.js
@@ -42,3 +42,59 @@ console.log(myShoes);
 //->Vans
 
 -------
+  
+SWITCH CASES IN FUNCTIONS
+
+function scaleOfFive(num){
+    var wellness = "";
+    switch(num){
+        case 0:
+        case 1: 
+            wellness = "Rest up, friend. Cheer up, eat your favorite ice cream and blast your music";
+        break;
+        case 2:
+        case 3:
+            wellness = "I hope doing okay, don't push yourself too hard. VIRTUAL 4 DIMENSIONAL HUGS"
+        break;
+        case 4:
+        case 5:
+            wellness = "I hope you have a great day/ mood tomorrow too! "
+            break;
+    }
+    return wellness;
+}
+
+/* Switch(){} is another way to write if statements. The case number are replacing what goes inside the parentheses, this case being num. So when I input
+1, it will return back what I wrote for case 1 considering that there is a return value inside my function. I wonder if I can replace the numbers with strings..
+*/ 
+
+//How would you rate your day on a scale of one to five?
+console.log(scaleOfFive(2));
+//-> I hope doing okay, don't push yourself too hard. VIRTUAL 4 DIMENSIONAL HUGS
+
+YOU CANNNN!!!!
+  
+function animegenre(genre) {
+    var type =""
+    switch(genre){
+        case "romance":
+          type="ao haru ride";
+          break;
+        case "magic":
+          type="black clover";
+          break;
+        case "mafia":
+          type="bungou stray dogs";
+        //this was on purpose
+          type="91 days";
+          break;
+        default:
+          type="something went wrong!";
+    }
+    return type;
+}
+
+// Oh and the default is like another case that's not defined. 
+
+console.log(animegenre("mafia")); 
+//-> 91 days
