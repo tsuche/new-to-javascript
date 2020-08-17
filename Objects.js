4db35e10534bfbe102416b70faf72a4352c40759
tsuche <67209810+tsuche@users.noreply.github.com>
2020-08-16 23:12:44 -0700

    Using brackets for strings w/ words

--- Objects.js
+++ Objects.js
@@ -8,9 +8,10 @@ var i = Math.floor(Math.random() * 2)
 
 
 var data = {
-  names: names[1],
-  color: "green",
+  "names": names[1],
+  "color": "green",
   number: [["17", "18"], ["10", "8", "21"]]
+  "long word": ["orthodontist", "infinity"]
 }
 
 var me = data.number[1];
@@ -20,3 +21,8 @@ console.log(me);
 var me = data.color;
 console.log(me);
 //-> "green
+
+var me = data["long word"][0];
+console.log(me);
+//-> need to use brackets when string is two letters cause can't write (data.long word) ya know?
+
